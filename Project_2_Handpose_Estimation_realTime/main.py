6fcfc08524f8473c18ee8175d44ec5d5d38ce9ab
Harsh <guptaharshbly@gmail.com>
2025-03-03 14:26:11 +0530

    Added Project 2: Handpose detection

--- Project_2_Handpose_Estimation_realTime/main.py
+++ Project_2_Handpose_Estimation_realTime/main.py
@@ -9,7 +9,6 @@ video =cv2.VideoCapture(0)
 with mp_hand.Hands(min_detection_confidence=0.5,
                min_tracking_confidence=0.5) as hands:
 
-
     while True:
         ret,image=video.read()
         image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
